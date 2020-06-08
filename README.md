8da1a97e06c3cd60bbfeca6ea6f1bf82d836e263
vera <marinenskaya.vera@gmail.com>
2020-06-08 14:03:49 +0400

    add parser yaml-files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/832a3c5606510eec990f/maintainability)](https://codeclimate.com/github/simpllex/python-project-lvl2/maintainability)
 
+[![Test Coverage](https://api.codeclimate.com/v1/badges/832a3c5606510eec990f/test_coverage)](https://codeclimate.com/github/simpllex/python-project-lvl2/test_coverage)
+
 [![Build Status](https://travis-ci.org/simpllex/python-project-lvl2.svg?branch=master)](https://travis-ci.org/simpllex/python-project-lvl2)
--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-
-"""The option parser."""
-
 import json
-from os import path
+import os
+import yaml
+
 
 
 def build_diff(old, new):
@@ -14,22 +12,30 @@ def build_diff(old, new):
     new_keys = set(new.keys())
     intersect_keys = old_keys & new_keys
     removed = old_keys - new_keys
-    removed = {o: old[o] for o in removed}
+    removed = {key: old[key] for key in removed}
     result += convert_to_string(removed)
     added = new_keys - old_keys
-    added = {o: new[o] for o in added}
+    added = {key: new[key] for key in added}
     result += convert_to_string(added)
-    modified_old = {o: old[o] for o in intersect_keys if old[o] != new[o]}
+    modified_old = {key: old[key] for key in intersect_keys if old[key] != new[key]}
     result += convert_to_string(modified_old)
-    modified_new = {o: new[o] for o in intersect_keys if old[o] != new[o]}
+    modified_new = {key: new[key] for key in intersect_keys if old[key] != new[key]}
     result += convert_to_string(modified_new)
-    same = {o: old[o] for o in intersect_keys if old[o] == new[o]}
+    same = {key: old[key] for key in intersect_keys if old[key] == new[key]}
     result += convert_to_string(same)
     return ("{\n" + result + "\n}")
 
 
-def read_file(file):
-    return json.load(open(path.abspath(file)))
+def read_file(path):
+    if not os.path.isabs(path):
+        path = os.path.join(os.getcwd(), path)
+    _, file_type = os.path.splitext(path)
+    if file_type == '.json':
+        return json.load(open(path))
+    elif file_type == '.yml':
+        return yaml.load(open(path), Loader=yaml.Loader)
+    else:
+        raise Exception("Invalid format, run --help!")
 
 
 def convert_to_string(my_dict):
